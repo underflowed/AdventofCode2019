675adbc8c2ea8c7b39df1fe4164b77044ec7d985
underflowed <40325724+underflowed@users.noreply.github.com>
2019-12-08 03:08:47 -0500

    added solution to day 2 part 2

--- day 2/Solution.java
+++ day 2/Solution.java
@@ -13,37 +13,58 @@ public class Solution {
         String input = bufferedReader.readLine();
         String[] filteredinput = input.split(",",0);
         // Replacement of position 1 and 2 as per request of the challenge
-        filteredinput[1] = "12";
-        filteredinput[2] = "2";
         int[] opcode = new int[filteredinput.length];
         int position1;
         int position2;
         int position3;
         int sum;
-        int multiplied; 
+        int multiplied;
+        int noun;
+        int verb; 
         for(int i = 0; i < filteredinput.length; i++) {
             opcode[i] = Integer.parseInt(filteredinput[i]);           
         }
-        for(int i = 0; i < opcode.length;) {
-            if(opcode[i] == 1) {
-                position1 = opcode[i + 1]; 
-                position2 = opcode[i + 2];
-                position3 = opcode[i + 3];
-                sum = opcode[position1] + opcode[position2];
-                opcode[position3] = sum;
-                i += 4;              
-            } else if(opcode[i] == 2) {
-                position1 = opcode[i + 1];
-                position2 = opcode[i + 2];
-                position3 = opcode[i + 3];
-                multiplied = opcode[position1] * opcode[position2];
-                opcode[position3] = multiplied;
-                i += 4;                
-            } else if(opcode[i] == 99) {
-                break;
-            } 
+        for(int i = 0; i < 100; i++){
+            noun = i;
+            for(int j = 0; j < 100; j++){
+                verb = j;
+                for(int k = 0; k < opcode.length;) {
+                    opcode[1] = noun;
+                    opcode[2] = verb;
+                    if(opcode[k] == 1) {
+                        position1 = opcode[k + 1]; 
+                        position2 = opcode[k + 2];
+                        position3 = opcode[k + 3];
+                        sum = opcode[position1] + opcode[position2];
+                        opcode[position3] = sum;
+                        k += 4;              
+                    } else if(opcode[k] == 2) {
+                        position1 = opcode[k + 1];
+                        position2 = opcode[k + 2];
+                        position3 = opcode[k + 3];
+                        multiplied = opcode[position1] * opcode[position2];
+                        opcode[position3] = multiplied;
+                        k += 4;                
+                    } else if(opcode[k] == 99) {
+                        break;
+                    }
+                 
+                }
+                if(opcode[0] == 19690720){
+                    System.out.println(100 * noun + verb);
+                    break;
+                } else {
+                    opcode = reset(opcode, filteredinput);
+                }
+            }
         }
-        System.out.println(opcode[0]);
         bufferedReader.close();
+    }
+
+    static int[] reset(int[] intarray, String[] stringarray) {
+        for(int i = 0; i < stringarray.length; i++) {
+            intarray[i] = Integer.parseInt(stringarray[i]);
+        }
+        return intarray;
     }
 }
